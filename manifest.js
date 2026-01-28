8e67290936b65e8f3fa65608ef24c9ffe03b7e22
chadney123 <chadney123@yahoo.com>
2026-01-28 22:50:31 +0000

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "short_name": "StaffClock",
+  "name": "Staff Clock-In System",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3239/3239147.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "./index.html",
+  "background_color": "#2563eb",
+  "display": "standalone",
+  "scope": "./",
+  "theme_color": "#2563eb"
+}
